3967b272ad13027bc1fb0acd34ccdcc55d9c1508
Jason N <fireworkinyourbathtub@gmail.com>
2022-11-04 19:36:02 -0400

    use ace, add missing files, remove js files

--- src/diagnostics.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.report_diagnostic = exports.Diagnostic = void 0;
-class Diagnostic {
-}
-exports.Diagnostic = Diagnostic;
-function report_diagnostic(diagnostic) {
-}
-exports.report_diagnostic = report_diagnostic;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import * as lexer from './lexer';
 
-document.querySelector('form')?.addEventListener('submit', function() {
+declare const ace: any;
+
+let editor = ace.edit("codeeditor");
+
+editor.setOption("printMarginColumn", false);
+
+document.getElementById('submitbutton')!.addEventListener('click', function() {
     let inputcodebox: any = document.getElementById('inputcodebox')!;
-    let input: string = inputcodebox.value;
+    let input: string = editor.getValue();
 
     document.getElementById("codeview")!.textContent = input;
 
