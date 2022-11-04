4e3dd6115ecd736702cebff5548529f682060f30
Jason N <fireworkinyourbathtub@gmail.com>
2022-11-04 19:38:00 -0400

    remove js files from gitignore

---
+++ src/diagnostics.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.report_diagnostic = exports.Diagnostic = void 0;
+class Diagnostic {
+}
+exports.Diagnostic = Diagnostic;
+function report_diagnostic(diagnostic) {
+}
+exports.report_diagnostic = report_diagnostic;
